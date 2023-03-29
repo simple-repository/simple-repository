6bf99ef5e32dac73857bca21453efca410cb855d
fiannacc <francesco.iannaccone@cern.ch>
2023-03-30 00:55:36 +0200

    edited package not found exception to be compatible with the new classes

--- acc_py_index/errors.py
+++ acc_py_index/errors.py
@@ -1,10 +1,12 @@
 class PackageNotFoundError(ValueError):
     msg_format = (
-        "Package '{package_name}' was not found in the configured sources: {sources}"
+        "Package '{package_name}' was not found in the configured source"
     )
 
-    def __init__(self, package_name: str, sources: list[str], *args: object):
-        package_name = package_name
-        sources = sources
-        msg = self.msg_format.format(package_name=package_name, sources=sources)
+    def __init__(self, package_name: str, *args: object):
+        msg = self.msg_format.format(package_name=package_name)
         super().__init__(msg, *args)
+
+
+class SourceRepositoryUnavailable(Exception):
+    pass
