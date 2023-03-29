061dc5fa48a08c64a6e1210580c2f0ce673ebf19
fiannacc <francesco.iannaccone@cern.ch>
2023-03-29 22:40:28 +0200

    Rollback pep-658 metadata support

--- acc_py_index/errors.py
+++ acc_py_index/errors.py
@@ -8,11 +8,3 @@ class PackageNotFoundError(ValueError):
         sources = sources
         msg = self.msg_format.format(package_name=package_name, sources=sources)
         super().__init__(msg, *args)
-
-class UnsupportedPackageFormat(ValueError):
-    msg_format = (
-        "Metadata extraction is not supported for this package: '{package_name}'"
-    )
-    def __init__(self, package_name: str, *args: object):
-        msg = self.msg_format.format(package_name=package_name)
-        super().__init__(msg, *args)
--- acc_py_index/tests/aiohttp_mock.py
+++ acc_py_index/tests/aiohttp_mock.py
@@ -1,12 +1,18 @@
+import types
 from unittest import mock
 
 
 class MockedRequestContextManager:
-    def __init__(self, response_mock: mock.Mock):
-        self.response_mock = response_mock
+    def __init__(self, response_mock: mock.Mock) -> None:
+        self.response_mock: mock.Mock = response_mock
 
-    async def __aenter__(self):
+    async def __aenter__(self) -> mock.Mock:
         return self.response_mock
 
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
+    async def __aexit__(
+        self,
+        exc_type: type,
+        exc_val: Exception,
+        exc_tb: types.TracebackType,
+    ) -> None:
         pass
