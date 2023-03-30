8ac7abed6f734a8c3d3f9576f072b7994cd9dd50
Francesco Iannaccone <francesco.iannaccone@cern.ch>
2023-03-30 16:48:38 +0200

    add model docs

--- acc_py_index/simple/model.py
+++ acc_py_index/simple/model.py
@@ -4,25 +4,33 @@ from typing import Optional
 
 @dataclass
 class File:
+    """Simple representation of a distribution file.
+    Defined in PEP-691: https://peps.python.org/pep-0691/
+    """
+
     filename: str
     url: str
     hashes: dict[str, str]
     requires_python: Optional[str] = None
-    dist_info_metadata: Optional[str] = None
+    dist_info_metadata: Optional[str] = None  # PEP-658
     gpg_sig: Optional[str] = None
-    yanked: Optional[str] = None
+    yanked: Optional[str] = None  # PEP-592: https://peps.python.org/pep-0592/
 
 
 @dataclass
 class Meta:
+    """Responses metadata defined in PEP-629:
+    https://peps.python.org/pep-0629/
+    """
     api_version: str
 
 
 @dataclass
 class ProjectDetail:
+    """Model of a project page as described in PEP-691"""
+    meta: Meta
     name: str
     files: list[File]
-    meta: Meta
 
 
 @dataclass
@@ -35,5 +43,6 @@ class ProjectListElement:
 
 @dataclass
 class ProjectList:
+    """Model of the project list as described in PEP-691"""
     meta: Meta
     projects: set[ProjectListElement]
