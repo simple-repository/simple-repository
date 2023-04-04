bc21b446c8879f94b37660aaed48210b8f4e3433
Francesco Iannaccone <francesco.iannaccone@cern.ch>
2023-04-04 11:48:03 +0200

    edited exception and content_type handling in http repo

--- acc_py_index/simple/repositories.py
+++ acc_py_index/simple/repositories.py
@@ -41,36 +41,46 @@ class HttpSimpleRepository(SimpleRepository):
 
         page_url = urljoin(self.source_url, f"{project_name}/")
         async with self.session.get(page_url, headers=headers) as response:
-            if 400 <= response.status <= 499:
+            if response.status == 404:
                 raise errors.PackageNotFoundError(
                     package_name=project_name,
                 )
+            response.raise_for_status()
 
             body: str = await response.text()
-            if "application/vnd.pypi.simple.v1+json" in response.headers.get("content-type", ""):
-                project_page: ProjectDetail = parser.parse_json_project_page(body)
-            else:
+            content_type = response.headers.get("content-type", "")
+
+            if (
+                "application/vnd.pypi.simple.v1+html" in content_type or
+                "text/html" in content_type or not content_type
+            ):
                 project_page = parser.parse_html_project_page(body, project_name)
+            elif "application/vnd.pypi.simple.v1+json" in content_type:
+                project_page = parser.parse_json_project_page(body)
+            else:
+                raise errors.UnsupportedSerialization()
 
             for file in project_page.files:
-                file.url = utils.url_absolutizer(
-                    file.url, page_url,
-                )
-
+                file.url = utils.url_absolutizer(file.url, page_url)
             return project_page
 
     async def get_project_list(self) -> ProjectList:
         headers = {"Accept": self.CONTENT_TYPES}
 
         async with self.session.get(self.source_url, headers=headers) as response:
-            if 400 <= response.status <= 499:
+            if response.status == 404:
                 raise errors.SourceRepositoryUnavailable()
+            response.raise_for_status()
 
             body = await response.text()
+            content_type = response.headers.get("content-type", "")
 
-            if "application/vnd.pypi.simple.v1+json" in response.headers.get("content-type", ""):
-                project_list: ProjectList = parser.parse_json_project_list(body)
-            else:
-                project_list = parser.parse_html_project_list(body)
+            if (
+                "application/vnd.pypi.simple.v1+html" in content_type or
+                "text/html" in content_type or not content_type
+            ):
+                return parser.parse_html_project_list(body)
+            elif "application/vnd.pypi.simple.v1+json" in content_type:
+                return parser.parse_json_project_list(body)
 
-            return project_list
+            raise errors.UnsupportedSerialization()
