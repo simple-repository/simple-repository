63ae2e790557ee99798d1fc9373d30178a085d97
Francesco Iannaccone <francesco.iannaccone@cern.ch>
2023-04-06 10:41:04 +0200

    modified model to follow PEP691

--- acc_py_index/simple/model.py
+++ acc_py_index/simple/model.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Union
 
 
 @dataclass
@@ -10,11 +10,29 @@ class File:
 
     filename: str
     url: str
-    hashes: dict[str, str]  # hash_name to hash_value mapping
+
+    # Dictionary mapping hash names to a hex encoded digest of the file's hash.
+    # Limited to a len() of 1 in HTML. Additional hashes are not included in
+    # the HTML serialization.
+    hashes: dict[str, str]
     requires_python: Optional[str] = None
-    dist_info_metadata: Optional[str] = None  # PEP-658
-    gpg_sig: Optional[str] = None
-    yanked: Optional[str] = None  # PEP-592: https://peps.python.org/pep-0592/
+
+    # If dist_info_metadata is present, it MUST be either a boolean to indicate
+    # if the file has an associated metadata file, or a dictionary mapping hash
+    # names to a hex encoded digest of the metadata’s hash.
+    dist_info_metadata: Optional[Union[bool, dict[str, str]]] = None  # PEP-658
+
+    # An optional key that acts a boolean to indicate if the file has an
+    # associated GPG signature or not. If this key does not exist, then the
+    # signature may or may not exist.
+    gpg_sig: Optional[bool] = None
+
+    # yanked, if present, may be either a boolean to indicate if the file has been
+    # yanked, or a non empty, but otherwise arbitrary, string to indicate that a file
+    # has been yanked with a specific reason. If the yanked key is present and is a
+    # truthy value, then it SHOULD be interpreted as indicating that the file pointed to
+    # by the url field has been “Yanked” as per PEP 592: https://peps.python.org/pep-0592/.
+    yanked: Optional[Union[bool, str]] = None
 
 
 @dataclass
