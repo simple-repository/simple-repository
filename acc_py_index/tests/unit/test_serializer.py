86ac046828bfb6946330fa06fbceb1c11b5750b1
Francesco Iannaccone <francesco.iannaccone@cern.ch>
2023-05-16 09:17:26 +0200

    refactor serializer, add json serializer

--- acc_py_index/tests/unit/test_serializer.py
+++ acc_py_index/tests/unit/test_serializer.py
@@ -1,12 +1,15 @@
+import json
 from typing import Optional, Union
 
 import pytest
 
-from acc_py_index.simple import serializer
 from acc_py_index.simple.model import File, Meta, ProjectDetail, ProjectList, ProjectListElement
+from acc_py_index.simple.serializer import SerializerHtmlV1, SerializerJsonV1
 
 
 def test_serialize_file_html() -> None:
+    serializer = SerializerHtmlV1()
+
     file = File(
         filename="test.html",
         url="https://example.com/test.html",
@@ -17,7 +20,7 @@ def test_serialize_file_html() -> None:
         '<a href="https://example.com/test.html#sha256=abc123" data-requires-python="&gt;=3.6"'
         '>test.html</a><br/>\n'
     )
-    assert serializer._serialize_file_html(file) == expected
+    assert serializer._serialize_file(file) == expected
 
     file = File(
         filename="test.html",
@@ -28,7 +31,7 @@ def test_serialize_file_html() -> None:
         '<a href="https://example.com/test.html"'
         '>test.html</a><br/>\n'
     )
-    assert serializer._serialize_file_html(file) == expected
+    assert serializer._serialize_file(file) == expected
 
 
 @pytest.mark.parametrize(
@@ -44,6 +47,9 @@ def test_serialize_file_html_yank(
     yank_attr: str,
     yank_value: Optional[Union[bool, str]],
 ) -> None:
+    serializer = SerializerHtmlV1(
+
+    )
     file = File(
         filename="test.html",
         url="https://example.com/test.html",
@@ -55,7 +61,7 @@ def test_serialize_file_html_yank(
         f'{yank_attr}'
         '>test.html</a><br/>\n'
     )
-    assert serializer._serialize_file_html(file) == expected
+    assert serializer._serialize_file(file) == expected
 
 
 @pytest.mark.parametrize(
@@ -71,6 +77,8 @@ def test_serialize_file_html_metadata(
     metadata_attr: str,
     metadata_value: Optional[Union[bool, dict[str, str]]],
 ) -> None:
+    serializer = SerializerHtmlV1()
+
     file = File(
         filename="test.html",
         url="https://example.com/test.html",
@@ -82,7 +90,7 @@ def test_serialize_file_html_metadata(
         f'{metadata_attr}'
         '>test.html</a><br/>\n'
     )
-    assert serializer._serialize_file_html(file) == expected
+    assert serializer._serialize_file(file) == expected
 
 
 @pytest.mark.parametrize(
@@ -94,6 +102,8 @@ def test_serialize_file_html_metadata(
     ],
 )
 def test_serialize_file_html_gpg(gpg_attr: str, gpg_value: Optional[bool]) -> None:
+    serializer = SerializerHtmlV1()
+
     file = File(
         filename="test.html",
         url="https://example.com/test.html",
@@ -105,7 +115,7 @@ def test_serialize_file_html_gpg(gpg_attr: str, gpg_value: Optional[bool]) -> No
         f'{gpg_attr}'
         '>test.html</a><br/>\n'
     )
-    assert serializer._serialize_file_html(file) == expected
+    assert serializer._serialize_file(file) == expected
 
 
 def test_serialize_project_page_html() -> None:
@@ -118,18 +128,19 @@ def test_serialize_project_page_html() -> None:
         ],
     )
     expected = """<!DOCTYPE html>
-<html>
-  <head>
-    <meta name="pypi:repository-version" content="1.0">
-    <title>Links for test-project</title>
-  </head>
-  <body>
+    <html>
+    <head>
+        <meta name="pypi:repository-version" content="1.0">
+        <title>Links for test-project</title>
+    </head>
+    <body>
     <h1>Links for test-project</h1>
 <a href="https://example.com/test.html">test.html</a><br/>
 <a href="test.txt">test.txt</a><br/>
 </body>
 </html>"""
-    assert serializer._serialize_project_page_html(project_page) == expected
+    serializer = SerializerHtmlV1()
+    assert serializer.serialize_project_page(project_page) == expected
 
 
 def test_serialize_project_list_html() -> None:
@@ -151,9 +162,96 @@ def test_serialize_project_list_html() -> None:
     expected_footer = """</body>
 </html>"""
 
-    serialized_page = serializer._serialize_project_list_html(project_list)
+    serializer = SerializerHtmlV1()
+    serialized_page = serializer.serialize_project_list(project_list)
     assert serialized_page.startswith(expected_header)
     assert serialized_page.endswith(expected_footer)
     a_tags = serialized_page.removeprefix(expected_header).removesuffix(expected_footer)
     assert '<a href="test-project-1/">test-project-1</a><br/>' in a_tags
     assert '<a href="test-project-2/">test-project-2</a><br/>' in a_tags
+
+
+def test_serialize_project_page_json() -> None:
+    page = ProjectDetail(
+        Meta("1.0"),
+        "project",
+        files=[
+            File(
+                filename="test1.whl",
+                url="test1.whl",
+                hashes={},
+            ),
+            File(
+                filename="test2.whl",
+                url="test2.whl",
+                hashes={"hash": "test_hash"},
+                requires_python=">4.0",
+                dist_info_metadata=True,
+                yanked="yanked",
+                gpg_sig=True,
+            ),
+            File(
+                filename="test3.whl",
+                url="test3.whl",
+                hashes={},
+                dist_info_metadata={"sha": "..."},
+                yanked=True,
+            ),
+        ],
+    )
+    serializer = SerializerJsonV1()
+    res = serializer.serialize_project_page(page)
+
+    assert json.loads(res) == json.loads('''
+        {
+            "meta": {
+                "api-version": "1.0"
+            },
+            "name": "project",
+            "files": [
+                {
+                    "filename": "test1.whl",
+                    "url": "test1.whl",
+                    "hashes": {}
+                },
+                {
+                    "filename": "test2.whl",
+                    "url": "test2.whl",
+                    "hashes": {"hash": "test_hash"},
+                    "requires-python": ">4.0",
+                    "dist-info-metadata": true,
+                    "yanked": "yanked",
+                    "gpg-sig": true
+                },
+                {
+                    "filename": "test3.whl",
+                    "url": "test3.whl",
+                    "hashes": {},
+                    "dist-info-metadata": {"sha": "..."},
+                    "yanked": true
+                }
+            ]
+        }
+    ''')
+
+
+def test_serialize_project_list_json() -> None:
+    page = ProjectList(
+        Meta("1.0"),
+        projects={
+            ProjectListElement("a"),
+        },
+    )
+    serializer = SerializerJsonV1()
+    res = serializer.serialize_project_list(page)
+
+    assert res == json.dumps(
+        json.loads('''{
+        "meta": {
+            "api-version": "1.0"
+        },
+        "projects": [
+            {"name": "a"}
+        ]
+    }'''),
+    )
