e4998eec7fbd0a6d5812fa64718a401823d5abaa
Francesco Iannaccone <francesco.iannaccone@cern.ch>
2023-05-19 16:10:29 +0200

    add UnsafeMergedRepo tests

---
+++ acc_py_index/tests/unit/test_unsafe_merged_repo.py
@@ -0,0 +1,74 @@
+import pytest
+
+from acc_py_index import errors
+from acc_py_index.simple.grouped_repository import UnsafeMergedRepo
+from acc_py_index.simple.model import File, Meta, ProjectDetail
+
+from ..mock_repository import MockRepository
+
+
+@pytest.mark.asyncio
+async def test_get_project_page() -> None:
+    repo = UnsafeMergedRepo(
+        [
+            MockRepository(),
+            MockRepository(
+                project_pages=[
+                    ProjectDetail(
+                        Meta('1.0'),
+                        "numpy",
+                        files=[
+                            File("numpy-1.1.whl", "url1", {}),
+                            File("numpy-1.2.whl", "url1", {}),
+                        ],
+                    ),
+                ],
+            ),
+            MockRepository(
+                project_pages=[
+                    ProjectDetail(
+                        Meta('1.0'),
+                        "numpy",
+                        files=[
+                            File("numpy-1.1.whl", "url2", {}),
+                            File("numpy-1.3.whl", "url2", {}),
+                        ],
+                    ),
+                ],
+            ),
+        ],
+    )
+
+    resp = await repo.get_project_page(project_name="numpy")
+
+    assert resp == ProjectDetail(
+        Meta('1.0'),
+        "numpy",
+        files=[
+            File("numpy-1.1.whl", "url1", {}),
+            File("numpy-1.2.whl", "url1", {}),
+            File("numpy-1.3.whl", "url2", {}),
+        ],
+    )
+
+
+@pytest.mark.asyncio
+async def test_get_project_page_failed() -> None:
+    repo = UnsafeMergedRepo([
+        MockRepository() for _ in range(3)
+    ])
+
+    with pytest.raises(
+        errors.PackageNotFoundError,
+        match="Package 'numpy' was not found in the configured source",
+    ):
+        await repo.get_project_page("numpy")
+
+
+@pytest.mark.asyncio
+async def test_not_normalized_package() -> None:
+    repo = UnsafeMergedRepo([
+        MockRepository() for _ in range(3)
+    ])
+    with pytest.raises(errors.NotNormalizedProjectName):
+        await repo.get_project_page("non_normalized")
