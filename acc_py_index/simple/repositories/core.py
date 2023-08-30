c1a2cc70bda9bc1d48995a45cf212170f3fe2d25
Francesco Iannaccone <francesco.iannaccone@cern.ch>
2023-08-30 15:43:58 +0200

    update SimpleRepository and RepositoryContainer interface

--- acc_py_index/simple/repositories/core.py
+++ acc_py_index/simple/repositories/core.py
@@ -1,16 +1,28 @@
 from typing import Protocol
 
-from ..model import ProjectDetail, ProjectList, Resource
+from .. import model
 
 
 class SimpleRepository(Protocol):
-    async def get_project_page(self, project_name: str) -> ProjectDetail:
+    async def get_project_page(
+        self,
+        project_name: str,
+        request_context: model.RequestContext,
+    ) -> model.ProjectDetail:
         ...
 
-    async def get_project_list(self) -> ProjectList:
+    async def get_project_list(
+        self,
+        request_context: model.RequestContext,
+    ) -> model.ProjectList:
         ...
 
-    async def get_resource(self, project_name: str, resource_name: str) -> Resource:
+    async def get_resource(
+        self,
+        project_name: str,
+        resource_name: str,
+        request_context: model.RequestContext,
+    ) -> model.Resource:
         ...
 
 
@@ -22,11 +34,23 @@ class RepositoryContainer(SimpleRepository):
     def __init__(self, source: SimpleRepository) -> None:
         self.source = source
 
-    async def get_project_page(self, project_name: str) -> ProjectDetail:
-        return await self.source.get_project_page(project_name)
+    async def get_project_page(
+        self,
+        project_name: str,
+        request_context: model.RequestContext,
+    ) -> model.ProjectDetail:
+        return await self.source.get_project_page(project_name, request_context)
 
-    async def get_project_list(self) -> ProjectList:
-        return await self.source.get_project_list()
+    async def get_project_list(
+        self,
+        request_context: model.RequestContext,
+    ) -> model.ProjectList:
+        return await self.source.get_project_list(request_context)
 
-    async def get_resource(self, project_name: str, resource_name: str) -> Resource:
-        return await self.source.get_resource(project_name, resource_name)
+    async def get_resource(
+        self,
+        project_name: str,
+        resource_name: str,
+        request_context: model.RequestContext,
+    ) -> model.Resource:
+        return await self.source.get_resource(project_name, resource_name, request_context)
