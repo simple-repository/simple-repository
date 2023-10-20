53e4523ea1f84b94aee55b5492229d9050501787
Francesco Iannaccone <francesco.iannaccone@cern.ch>
2023-10-20 11:45:48 +0200

    refactor YankRepository to make it more flexible

---
+++ simple_repository/tests/components/test_yank_providers.py
@@ -0,0 +1,130 @@
+import pathlib
+
+import aiosqlite
+import pytest
+
+from ... import errors, model
+from ...components.yanking import GlobYankProvider, SqliteYankProvider
+
+
+@pytest.fixture
+def project_page() -> model.ProjectDetail:
+    return model.ProjectDetail(
+        model.Meta("1.0"),
+        "project",
+        (
+            model.File("project-1.0.whl", "url", {}),
+            model.File("project-1.0.tar.gz", "url", {}),
+        ),
+    )
+
+
+@pytest.mark.asyncio
+async def test_glob_provider__yanked_files(
+    tmp_path: pathlib.Path,
+    project_page: model.ProjectDetail,
+) -> None:
+    file = tmp_path / "yank_config.json"
+    file.write_text(
+        data='{"project": ["*.whl", "bad"], "pandas": ["*[!.whl]", "not supported"]}',
+    )
+
+    provider = GlobYankProvider(yank_config_file=file)
+
+    res = await provider.yanked_files(project_page)
+    assert {'project-1.0.whl': 'bad'} == res
+
+
+@pytest.mark.asyncio
+async def test_glob_provider__yanked_versions(
+    tmp_path: pathlib.Path,
+    project_page: model.ProjectDetail,
+) -> None:
+    file = tmp_path / "yank_config.json"
+    file.write_text(
+        data='{"numpy": ["*.whl", "bad"], "pandas": ["*[!.whl]", "not supported"]}',
+    )
+
+    provider = GlobYankProvider(yank_config_file=file)
+
+    res = await provider.yanked_versions(project_page)
+    assert {} == res
+
+
+@pytest.mark.parametrize(
+    "json_string", [
+        "42", "true", '["a", "b"]', "null", '"ciao"',
+    ],
+)
+def test_load_config_wrong_type(
+    json_string: str,
+    tmp_path: pathlib.PosixPath,
+) -> None:
+    file = tmp_path / "yank_config.json"
+    file.write_text(
+        data=json_string,
+    )
+    with pytest.raises(
+        errors.InvalidConfigurationError,
+        match=(
+            f"Invalid configuration file. {str(file)} must contain a dictionary."
+        ),
+    ):
+        GlobYankProvider(yank_config_file=file)
+
+
+def test_load_config_wrong_format(
+    tmp_path: pathlib.PosixPath,
+) -> None:
+    file = tmp_path / "yank_config.json"
+    file.write_text(
+        data='{"a": "b"}',
+    )
+    with pytest.raises(
+        errors.InvalidConfigurationError,
+        match=(
+            f'Invalid yank configuration file. {str(file)} must'
+            ' contain a dictionary mapping a project name to a tuple'
+            ' containing a glob pattern and a yank reason.'
+        ),
+    ):
+        GlobYankProvider(yank_config_file=file)
+
+
+def test_load_config_malformed_json(
+    tmp_path: pathlib.PosixPath,
+) -> None:
+    file = tmp_path / "yank_config.json"
+    file.write_text(
+        data='a',
+    )
+    with pytest.raises(
+        errors.InvalidConfigurationError,
+        match="Invalid json file",
+    ):
+        GlobYankProvider(yank_config_file=file)
+
+
+@pytest.mark.asyncio
+async def test_sqlite_provider__yanked_versions(
+    tmp_path: pathlib.Path,
+    project_page: model.ProjectDetail,
+) -> None:
+    database_path = tmp_path / "temp.db"
+    async with aiosqlite.connect(database_path) as database:
+        provider = SqliteYankProvider(database)
+        await provider._init_db()
+        await provider._database.executemany(
+            "INSERT INTO yanked_versions (project_name, version, reason)"
+            " VALUES(:project_name, :version, :reason)",
+            [
+                {"project_name": "project", "version": "1.0", "reason": "reason1"},
+                {"project_name": "project", "version": "2.0", "reason": "reason2"},
+            ],
+        )
+
+        await provider._database.commit()
+
+        versions = await provider.yanked_versions(project_page)
+
+    assert versions == {"1.0": "reason1", "2.0": "reason2"}
--- simple_repository/tests/components/test_yank_repository.py
+++ simple_repository/tests/components/test_yank_repository.py
@@ -1,145 +1,66 @@
-import pathlib
-from typing import Optional, Union
-from unittest import mock
-
-import aiosqlite
 import pytest
 
 from ... import model
-from ...components import yanking as yank_repository
+from ...components.yanking import YankProvider, YankRepository
 from .fake_repository import FakeRepository
 
 
-def test_yank_per_version() -> None:
-    project_page = model.ProjectDetail(
-        name="Project",
-        meta=model.Meta("1.0"),
-        files=(
-            model.File(
-              filename="Project-1.0.0-any.whl",
-              url="url",
-              hashes={},
-            ),
-            model.File(
-              filename="Project-1.0.0.tar.gz",
-              url="url",
-              hashes={},
-            ),
-            model.File(
-              filename="Project-1.0.1.tar.gz",
-              url="url",
-              hashes={},
-            ),
+class FakeYankProvider(YankProvider):
+    async def yanked_versions(self, project_page: model.ProjectDetail) -> dict[str, str]:
+        return {"1.0": "reason"}
+
+    async def yanked_files(self, project_page: model.ProjectDetail) -> dict[str, str]:
+        return {"project-1.0-any.whl": "reason"}
+
+
+@pytest.fixture
+def project_page() -> model.ProjectDetail:
+    return model.ProjectDetail(
+        model.Meta("1.0"), name="project", files=(
+            model.File("project-1.0-any.whl", "url", {}),
+            model.File("project-1.0.tar.gz", "url", {}),
+            model.File("project-1.1.tar.gz", "url", {}),
         ),
     )
 
-    yanked_versions = {"1.0.0": "reason"}
-    yanked_page = yank_repository.add_yanked_attribute_per_version(
-        project_page,
-        yanked_versions,
+
+@pytest.fixture
+def repository(project_page: model.ProjectDetail) -> YankRepository:
+    source = FakeRepository(
+        project_pages=[project_page],
+    )
+    provider = FakeYankProvider()
+
+    return YankRepository(
+        source=source,
+        yank_provider=provider,
+    )
+
+
+def test_yank_per_version(repository: YankRepository, project_page: model.ProjectDetail) -> None:
+    yanked_page = repository._add_yanked_attribute_per_version(
+        project_page=project_page,
+        yanked_versions={"1.0": "reason"},
     )
     assert yanked_page.files[0].yanked == "reason"
     assert yanked_page.files[1].yanked == "reason"
     assert yanked_page.files[2].yanked is None
 
 
-def test_yank_per_file() -> None:
-    project_page = model.ProjectDetail(
-        name="Project",
-        meta=model.Meta("1.0"),
-        files=(
-            model.File(
-              filename="project-1.0.0-any.whl",
-              url="url",
-              hashes={},
-            ),
-            model.File(
-              filename="Project-1.0.0.tar.gz",
-              url="url",
-              hashes={},
-            ),
-            model.File(
-              filename="Project-1.0.1.tar.gz",
-              url="url",
-              hashes={},
-            ),
-        ),
-    )
-
-    yanked_versions = {"project-1.0.0-any.whl": "reason"}
-    yanked_page = yank_repository.add_yanked_attribute_per_file(
-        project_page,
-        yanked_versions,
+def test_yank_per_file(repository: YankRepository, project_page: model.ProjectDetail) -> None:
+    yanked_page = repository._add_yanked_attribute_per_file(
+        project_page=project_page,
+        yanked_files={"project-1.0-any.whl": "reason"},
     )
     assert yanked_page.files[0].yanked == "reason"
     assert yanked_page.files[1].yanked is None
     assert yanked_page.files[2].yanked is None
 
 
-@pytest.mark.parametrize(
-    "yanked_versions, yanked_value", [
-        ({}, None),
-        ({"1.0": "reason"}, "reason"),
-        ({"1.0": ""}, True),
-    ],
-)
 @pytest.mark.asyncio
-async def test_get_project_page(
-    yanked_versions: dict[str, str],
-    yanked_value: Optional[Union[bool, str]],
-) -> None:
-    repository = yank_repository.YankRepository(
-        FakeRepository(
-            project_pages=[
-                model.ProjectDetail(
-                    model.Meta("1.0"), name="project", files=(
-                        model.File("project-1.0-anylinux.whl", "url", {}),
-                        model.File("project-1.0.tar.gz", "url", {}),
-                        model.File("project-1.1.tar.gz", "url", {}),
-                    ),
-                ),
-            ],
-        ),
-        mock.AsyncMock(),
-    )
+async def test_get_project_page(repository: YankRepository) -> None:
+    result = await repository.get_project_page("project")
 
-    with mock.patch(
-        "simple_repository.components.yanking.get_yanked_versions",
-        return_value=yanked_versions,
-    ):
-        result = await repository.get_project_page("project")
-
-    assert result.files[0].yanked == yanked_value
-    assert result.files[1].yanked == yanked_value
+    assert result.files[0].yanked == "reason"
+    assert result.files[1].yanked == "reason"
     assert result.files[2].yanked is None
-
-
-@pytest.mark.asyncio
-async def test_get_yanked_versions(tmp_path: pathlib.Path) -> None:
-    database_path = tmp_path / "temp.db"
-    async with aiosqlite.connect(database_path) as database:
-        await yank_repository.YankRepository(
-            source=FakeRepository(),
-            database=database,
-        )._init_db()
-        await database.execute(
-            "CREATE TABLE IF NOT EXISTS yanked_versions"
-            "(project_name TEXT, version TEXT, reason TEXT"
-            ", CONSTRAINT pk PRIMARY KEY (project_name, version))",
-        )
-        await database.execute(
-            "INSERT INTO yanked_versions (project_name, version, reason)"
-            " VALUES(:project_name, :version, :reason)",
-            {"project_name": "project", "version": "1.0", "reason": "reason1"},
-        )
-        await database.execute(
-            "INSERT INTO yanked_versions (project_name, version, reason)"
-            " VALUES(:project_name, :version, :reason)",
-            {"project_name": "project", "version": "2.0", "reason": "reason2"},
-        )
-
-        await database.commit()
-
-        versions = await yank_repository.get_yanked_versions("project", database)
-
-    assert versions == {"1.0": "reason1", "2.0": "reason2"}
