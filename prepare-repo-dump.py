e54cb33e17ba04eebc4f16bbb98c78af061f3b08
Phil Elson <philip.elson@cern.ch>
2024-09-10 13:46:40 +0200

    Improve the preparation for open-sourcing

--- prepare-repo-dump.py
+++ prepare-repo-dump.py
@@ -28,3 +28,17 @@ pyproject_content['project']['urls']['Homepage'] = (
     "https://github.com/simple-repository/simple-repository"
 )
 pyproject_path.write_text(tomlkit.dumps(pyproject_content) + '\n')
+
+for path in build.glob('**/*.py'):
+    content = path.read_text()
+    content = '\n'.join([
+        '# Copyright (C) 2023, CERN',
+        '# This software is distributed under the terms of the MIT',
+        '# licence, copied verbatim in the file "LICENSE".',
+        '# In applying this license, CERN does not waive the privileges and immunities',
+        '# granted to it by virtue of its status as Intergovernmental Organization',
+        '# or submit itself to any jurisdiction.',
+        '',
+    ]) + '\n' + content.lstrip()
+    content = content.rstrip() + '\n'
+    path.write_text(content, 'utf-8')
