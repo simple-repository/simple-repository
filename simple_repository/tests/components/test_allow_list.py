d9f957798b386043847156dde4716419f1b90dd6
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2024-09-18 15:55:40 +0200

    Reuse fixtures in tests

--- simple_repository/tests/components/test_allow_list.py
+++ simple_repository/tests/components/test_allow_list.py
@@ -1,46 +1,14 @@
 from __future__ import annotations
 
+import typing
+
 import pytest
 
 from ... import errors, model
 from ...components.allow_list import AllowListRepository
-from .fake_repository import FakeRepository
 
-
-@pytest.fixture
-def source_repository() -> FakeRepository:
-    return FakeRepository(
-        project_list=model.ProjectList(
-            meta=model.Meta("1.0"),
-            projects=frozenset([
-                model.ProjectListElement("project1"),
-                model.ProjectListElement("project2"),
-                model.ProjectListElement("project3"),
-            ]),
-        ),
-        project_pages=[
-            model.ProjectDetail(
-                meta=model.Meta("1.0"),
-                name="project1",
-                files=(),
-            ),
-            model.ProjectDetail(
-                meta=model.Meta("1.0"),
-                name="project2",
-                files=(),
-            ),
-            model.ProjectDetail(
-                meta=model.Meta("1.0"),
-                name="project3",
-                files=(),
-            ),
-        ],
-        resources={
-            "project1-1.0.tar.gz": model.HttpResource("content1"),
-            "project2-1.0.tar.gz": model.HttpResource("content2"),
-            "project3-1.0.tar.gz": model.HttpResource("content3"),
-        },
-    )
+if typing.TYPE_CHECKING:
+    from .fake_repository import FakeRepository
 
 
 @pytest.fixture
